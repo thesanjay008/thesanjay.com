50748ce0376e0899e60d96e5769ea2fe56bcba01
thesanjay008 <76848496+thesanjay008@users.noreply.github.com>
2022-05-08 16:17:27 +0530

    Delete application directory

--- application/views/admin/footer.php
+++
@@ -1,7 +0,0 @@
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      Made with <i class="fa fa-heart-o"></i> by <a href="https://www.thesanjay.com" target="_blank"><b>Sanjay Dhamecha</b></a>
-    </div>
-    <strong>Copyright &copy; 2015 - <?php echo date("Y"); ?> <a href="https://www.techstc.com" target="_blank"> TECH STC</a>.</strong> All rights
-    reserved.
-  </footer>
--- application/views/thesanjay/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-include_once('coreData.php');
-include_once('header.php');
-include_once('navigation.php');
-?>
-
-<?php include_once('footer.php'); ?>
